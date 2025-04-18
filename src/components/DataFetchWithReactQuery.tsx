cbe5ec6ef9fb91963297fb32cdb5deefd2be55d4
Isaac Delgado <isaacdelgado1994@gmail.com>
2025-04-18 01:37:47 -0700

    working

--- src/components/DataFetchWithReactQuery.tsx
+++ src/components/DataFetchWithReactQuery.tsx
@@ -1,10 +1,19 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchTodos } from "../api";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { addTodo, fetchTodos } from "../api";
+import { useState } from "react";
 
 export default function DataFetchWithReactQuery() {
+  const queryClient = useQueryClient();
   const { data: todos, isLoading } = useQuery({
     queryFn: () => fetchTodos(),
     queryKey: ["todos"],
+  });
+  const [title, setTitle] = useState("");
+  const { mutateAsync: addTodoMutation } = useMutation({
+    mutationFn: addTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["todos"]);
+    },
   });
 
   if (isLoading) {
@@ -13,6 +22,26 @@ export default function DataFetchWithReactQuery() {
 
   return (
     <>
+      <div>
+        <input
+          className="border-4"
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            try {
+              await addTodoMutation({ title });
+              setTitle("");
+            } catch (e) {
+              console.log(e);
+            }
+          }}
+        >
+          Add Todo
+        </button>
+      </div>
       <div>
         {todos?.map((todo) => {
           return <li key={todo.id}>{todo.title}</li>;
